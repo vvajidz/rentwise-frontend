79d822483f28d3953b688a45e406f88e465be385
Abdul Vajid <vvajidz56@gmail.com>
2025-11-19 11:35:17 +0530

    redeploy

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL, // from your .env
+  baseURL: "https://rentwise-backend-1-1ffp.onrender.com/api", 
   withCredentials: true, // so cookies (session, etc.) work
 });
 
